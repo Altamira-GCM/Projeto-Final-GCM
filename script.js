b8497eec1969ca0c7b7e4a9425bbd9e4b6ea3a8e
Gustavo <Gustavo.cesariosilva15@gmail.com>
2023-07-02 15:38:15 -0300

    Adicionando script para exibir a hora em que o site foi acessado

---
+++ script.js
@@ -0,0 +1,16 @@
+function exibirHoraAtual() {
+    let data = new Date();
+    let hora = data.getHours();
+    let minutos = data.getMinutes();
+    let segundos = data.getSeconds();
+  
+    hora = hora < 10 ? "0" + hora : hora;
+    minutos = minutos < 10 ? "0" + minutos : minutos;
+    segundos = segundos < 10 ? "0" + segundos : segundos;
+  
+    let horaAtual = hora + ":" + minutos + ":" + segundos;
+    document.querySelector("#hora-atual").innerHTML = `Acesso em ${horaAtual}`;
+}
+  
+exibirHoraAtual();
+  
