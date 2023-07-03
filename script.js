698b40efc0c63707e77cc8ff6cdb93167d345718
Gustavo <Gustavo.cesariosilva15@gmail.com>
2023-07-03 20:56:27 -0300

    Adicionando Banners relacionados à processos seletivos

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function exibirHoraAtual() {
     segundos = segundos < 10 ? "0" + segundos : segundos;
   
     let horaAtual = hora + ":" + minutos + ":" + segundos;
-    document.querySelector("#hora-atual").innerHTML = `Acesso em ${horaAtual}`;
+    document.querySelector("#hora-atual").innerHTML = `Último acesso em ${horaAtual}`;
 }
   
 exibirHoraAtual();
